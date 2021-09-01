a696fe5779885eaaa00044c2b3b74560698b96ec
Nick Gorjestani <nickg2794@hotmail.com>
2021-08-31 23:32:17 -0500

    Initial commit

--- src/components/AppNavbar.tsx
+++ src/components/AppNavbar.tsx
@@ -8,12 +8,7 @@ interface IAppNavbarProps {
 export const AppNavbar: FunctionComponent<IAppNavbarProps> = (props) => {
     return (
         <Navbar bg="primary" variant="dark">
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
+            <Navbar.Brand href="#home">Yellowstone</Navbar.Brand>
             <Form inline>
                 <FormControl type="text" placeholder="Search" className="mr-sm-2"/>
                 <Button variant="outline-light">Search</Button>
